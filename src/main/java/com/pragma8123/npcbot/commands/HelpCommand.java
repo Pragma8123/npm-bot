78dd60e5ca9b20e6476cb3cdea3234cd3c3880ea
Max Machon <max@machon.family>
2022-07-03 13:28:37 -0400

    Add start for "/help" command

---
+++ src/main/java/com/pragma8123/npcbot/commands/HelpCommand.java
@@ -0,0 +1,33 @@
+package com.pragma8123.npcbot.commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.discordjson.json.ApplicationCommandRequest;
+import discord4j.discordjson.json.ImmutableApplicationCommandRequest;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class HelpCommand implements SlashCommand {
+
+    private final String name = "help";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public ImmutableApplicationCommandRequest getApplicationCommandRequest() {
+        return ApplicationCommandRequest.builder()
+                .name(name)
+                .description("Get helpful information about the bot")
+                .build();
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        return event
+                .reply("Here is some helpful info")
+                .withEphemeral(true);
+    }
+}
