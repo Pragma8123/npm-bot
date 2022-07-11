a785ce199ea09167e7f75c825322f3d7dd86241e
Max Machon <max@machon.family>
2022-07-11 10:44:07 -0400

    Modify OpenAiConfigurationTest to make it easier to add more tests

--- src/test/java/com/pragma8123/npcbot/openai/OpenAiConfigurationTest.java
+++ src/test/java/com/pragma8123/npcbot/openai/OpenAiConfigurationTest.java
@@ -1,5 +1,6 @@
 package com.pragma8123.npcbot.openai;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,13 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OpenAiConfigurationTest {
 
-    ApplicationContextRunner context = new ApplicationContextRunner()
-            .withUserConfiguration(OpenAiConfiguration.class);
+    ApplicationContextRunner context;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.context = new ApplicationContextRunner()
+                .withUserConfiguration(OpenAiConfiguration.class);
+    }
 
     @Test
     public void ensureWebClientBean() {
-        context.run(it -> {
-            assertThat(it).hasSingleBean(WebClient.class);
-        });
+        context.run(it -> assertThat(it).hasSingleBean(WebClient.class));
     }
 }
