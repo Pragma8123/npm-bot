68689ea8b21b8e463058228fafca2a8507194993
Max Machon <max@machon.family>
2022-07-26 17:26:06 -0400

    Adjust our response timeout for the DALL-E API
    This will allow the DALL-E engine as much time as possible to generate or image(s). We chose the timeout of 14 minutes as Discord only allows up 15 minute to edit our reply.

--- src/main/java/com/pragma8123/npcbot/dalle/DallEConfiguration.java
+++ src/main/java/com/pragma8123/npcbot/dalle/DallEConfiguration.java
@@ -1,9 +1,14 @@
 package com.pragma8123.npcbot.dalle;
 
+import io.netty.channel.epoll.EpollChannelOption;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class DallEConfiguration {
@@ -13,7 +18,17 @@ public class DallEConfiguration {
 
     @Bean
     public WebClient dallEWebClient() {
+        // Responses from our API can take significant amounts of time in worst case scenarios
+        HttpClient client = HttpClient.create()
+                .option(EpollChannelOption.SO_KEEPALIVE, true)
+                .option(EpollChannelOption.TCP_KEEPIDLE, 840)
+                .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+                .option(EpollChannelOption.TCP_KEEPCNT, 14)
+                // 14 Minutes - We have to update our reply to discord within 15 minutes
+                .responseTimeout(Duration.ofSeconds(840L));
+
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(client))
                 .baseUrl(dallEApiBaseUrl)
                 .build();
     }
