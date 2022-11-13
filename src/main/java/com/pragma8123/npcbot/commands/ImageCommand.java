78a0720a0831ccf46c5fb30d2f16e642530f89dc
Max Machon <max@machon.family>
2022-11-13 13:09:44 -0500

    Replace DALL-E 1.0 with DALL-E 2.0

--- src/main/java/com/pragma8123/npcbot/commands/ImageCommand.java
+++ src/main/java/com/pragma8123/npcbot/commands/ImageCommand.java
@@ -1,13 +1,14 @@
 package com.pragma8123.npcbot.commands;
 
 import com.pragma8123.npcbot.NpcBotConstants;
-import com.pragma8123.npcbot.dalle.DallEService;
+import com.pragma8123.npcbot.openai.OpenAiService;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.core.spec.InteractionReplyEditSpec;
+import discord4j.discordjson.json.ApplicationCommandOptionChoiceData;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.discordjson.json.ImmutableApplicationCommandRequest;
@@ -27,11 +28,11 @@ public class ImageCommand implements SlashCommand {
 
     private final String name = "image";
 
-    private final DallEService dallEService;
+    private final OpenAiService openAiService;
 
     @Autowired
-    public ImageCommand(DallEService dallEService) {
-        this.dallEService = dallEService;
+    public ImageCommand(OpenAiService openAiService) {
+        this.openAiService = openAiService;
     }
 
     @Override
@@ -43,12 +44,30 @@ public class ImageCommand implements SlashCommand {
     public ImmutableApplicationCommandRequest getApplicationCommandRequest() {
         return ApplicationCommandRequest.builder()
                 .name(name)
-                .description("Generate an image based on your prompt using the DALL-E image engine")
+                .description("Generate an HD image based on your prompt using the OpenAI DALL-E 2.0 image engine")
                 .addOption(ApplicationCommandOptionData.builder()
                         .name("prompt")
                         .description("Prompt used to generate your image")
                         .type(ApplicationCommandOption.Type.STRING.getValue())
                         .required(true)
+                        .build())
+                .addOption(ApplicationCommandOptionData.builder()
+                        .name("size")
+                        .description("The size of the generated images")
+                        .type(ApplicationCommandOption.Type.STRING.getValue())
+                        .required(false)
+                        .addChoice(ApplicationCommandOptionChoiceData.builder()
+                                .name("small")
+                                .value("256x256")
+                                .build())
+                        .addChoice(ApplicationCommandOptionChoiceData.builder()
+                                .name("medium")
+                                .value("512x512")
+                                .build())
+                        .addChoice(ApplicationCommandOptionChoiceData.builder()
+                                .name("large")
+                                .value("1024x1024")
+                                .build())
                         .build())
                 .build();
     }
@@ -60,22 +79,30 @@ public class ImageCommand implements SlashCommand {
                 .map(ApplicationCommandInteractionOptionValue::asString)
                 .orElse("");
 
+        String size = event.getOption("size")
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asString)
+                .orElse("512x512");
+
+        String user = event.getInteraction().getUser().getId().asString();
+
         return event
                 .deferReply()
-                .then(dallEService.generateImage(prompt))
-                .flatMap(dallEResponse -> event.editReply(InteractionReplyEditSpec.builder()
+                .then(openAiService.getImage(prompt, 1L, size, user))
+                .flatMap(imageResponse -> event.editReply(InteractionReplyEditSpec.builder()
                         .addFile(
                                 "image.jpg",
-                                new ByteArrayInputStream(Base64.getDecoder().decode(dallEResponse.getImages().get(0))))
+                                new ByteArrayInputStream(Base64.getDecoder().decode(imageResponse.getImages().get(0).getB64Data()))
+                        )
                         .addEmbed(EmbedCreateSpec.builder()
-                                .description("`" + prompt + "`")
+                                .title(prompt)
                                 .color(NpcBotConstants.NPC_COLOR)
                                 .image("attachment://image.jpg")
                                 .build())
                         .build()))
                 .onErrorResume(error -> {
                     LOG.error(error.toString());
-                    return event.editReply("There was an error generating your image!");
+                    return event.editReply("There was an error generating your image");
                 })
                 .then();
     }
